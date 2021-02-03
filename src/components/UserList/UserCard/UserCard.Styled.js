fc4c35ce4ab80d99f29afdaa799bd057d0cfaf24
Mateusz Mikulski <mikulskee@gmail.com>
2021-02-03 07:28:03 +0100

    add mobile styles

--- src/components/UserList/UserCard/UserCard.Styled.js
+++ src/components/UserList/UserCard/UserCard.Styled.js
@@ -2,28 +2,46 @@ import styled from 'styled-components';
 
 export const UserCardWrapper = styled.li`
 	margin-bottom: 20px;
-	min-width: 500px;
 	margin-left: 20px;
+
+	@media only screen and (min-width: 1024px) {
+		min-width: 500px;
+	}
 	.user-name {
 		padding: 10px;
 		position: relative;
 		font-weight: 600;
-		font-size: 20px;
 		cursor: pointer;
+		font-size: 12px;
+
 		transition: opacity 0.15s linear;
+		@media only screen and (min-width: 1024px) {
+			font-size: 20px;
+		}
 		&:hover {
 			opacity: 0.7;
 		}
 		span {
+			display: block;
 			color: gray;
-			font-size: 15px;
-			margin-left: 10px;
+			font-size: 8px;
+			margin-left: 5px;
+
+			@media only screen and (min-width: 1024px) {
+				font-size: 15px;
+				margin-left: 10px;
+				display: inline;
+			}
 		}
 		svg {
 			position: absolute;
-			transform: translateY(-5px);
+			transform: translateY(-90%);
+			top: 50%;
 			right: 0;
 			width: 15px;
+			@media only screen and (min-width: 1024px) {
+				transform: translateY(-70%);
+			}
 		}
 	}
 
